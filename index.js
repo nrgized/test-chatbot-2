969ab0334f2474013f33d086bc5531398b58a53f
nrgized <Audrius@Audriuss-MacBook-Pro.local>
2016-07-19 23:38:21 +0300

    zone-details

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function getStopsData() {
       var cheerio = require('cheerio'),
       $ = cheerio.load(body);
       $( "ul.zones-list" ).children().each(function(i, elem) {
-        var name = $(this).find('.zone-name').text();
+        var name = $(this).find( ".zone-details" ).attr('title');
         var id = $(this).find( "input[name*='zone-id']" ).attr('value');
         console.log(id);
         console.log(name);
