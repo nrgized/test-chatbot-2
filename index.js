0b6db8bd58292c4ba69ca163d8a75de29f227bc0
nrgized <energieee@gmail.com>
2016-07-20 12:50:35 +0300

    error fix in LT char strip

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function getStopsData() {
         if($(this).find('.zone-details').length != 0)  {
           zone.nameFull = $(this).find( ".zone-details" ).attr('title');
           zone.nameLT = zone.nameFull.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-          zone.nameEN = zone.nameLT.ltstring.replace(/ą/gi,'a').replace(/č/gi, 'c').replace(/ę|ė/gi, 'e').replace(/į/gi, 'i').replace(/ų|ū/gi, 'u').replace(/ž/gi, 'z');
+          zone.nameEN = zone.nameLT.replace(/ą/gi,'a').replace(/č/gi, 'c').replace(/ę|ė/gi, 'e').replace(/į/gi, 'i').replace(/ų|ū/gi, 'u').replace(/ž/gi, 'z');
           zone.id = $(this).find( "input[name*='zone-id']" ).attr('value');
           //console.log(id + " " + name);
           zones.push(zone);
