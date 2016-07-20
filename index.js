0680c4aab0d664fb2f5747767d09b2ce4ebd9480
nrgized <energieee@gmail.com>
2016-07-20 13:51:24 +0300

    sh letter added to stripping

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function getStopsData() {
         if($(this).find('.zone-details').length != 0)  {
           zone.nameFull = $(this).find( ".zone-details" ).attr('title');
           zone.nameLT = zone.nameFull.replace(/[`„“~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-          zone.nameEN = zone.nameLT.replace(/ą/gi,'a').replace(/č/gi, 'c').replace(/ę|ė/gi, 'e').replace(/į/gi, 'i').replace(/ų|ū/gi, 'u').replace(/ž/gi, 'z').replace(/"/gi, '');
+          zone.nameEN = zone.nameLT.replace(/ą/gi,'a').replace(/č/gi, 'c').replace(/ę|ė/gi, 'e').replace(/į/gi, 'i').replace(/š/gi, 's').replace(/ų|ū/gi, 'u').replace(/ž/gi, 'z').replace(/"/gi, '');
           zone.id = $(this).find( "input[name*='zone-id']" ).attr('value');
           //console.log(id + " " + name);
           zones.push(zone);
