0c9c442febf650a4e3233a269515689074af774a
nrgized <energieee@gmail.com>
2016-07-21 16:13:59 +0300

    insert query

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ function getStopsData() {
       //connection.query('INSERT INTO `qll8yu61wian72aj`.`stop_names` (`index`, `nameLT`, `nameEN`) VALUES (9, 'asdfa', 'dddd');', function(err, rows, fields) {
       //connection.query("INSERT INTO `stop_names` (`index`, `nameLT`, `nameEN`) VALUES (12, 'asdfa', 'dddd');", function(err, rows, fields) {
       //connection.query('SELECT * FROM stop_names;', function(err, rows, fields) {
-      connection.query('DELETE * FROM stop_names;', function(err, rows, fields) {
-      //connection.query("INSERT INTO `stop_names` (`index`, `nameLT`, `nameEN`, `id`) VALUES " + queryStringValues + ";", function(err, rows, fields) {
+      //connection.query('DELETE * FROM stop_names;', function(err, rows, fields) {
+      connection.query("INSERT INTO `stop_names` (`index`, `nameLT`, `nameEN`, `id`) VALUES " + queryStringValues + ";", function(err, rows, fields) {
       if (err) throw err;
       console.log(rows);
       });
