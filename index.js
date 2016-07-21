80b788971e796e016de04f2f4cc3ffd9b99d8e1b
nrgized <energieee@gmail.com>
2016-07-21 17:43:03 +0300

    more values

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function getStopsData() {
       queryStringValues = queryStringValues.substring(0, queryStringValues.length - 1);
       
       // var queryString = 'INSERT INTO stop_names (index, nameLT, nameEN, id) VALUES ' + queryStringValues + ';';
-      var queryString = "INSERT INTO `stop_names` (`index`, `nameLT`, `nameEN`) VALUES (17, 'asdfa', 'dddd'), (18, 'asdfa', 'dddd');"
+      var queryString = "INSERT INTO `stop_names` (`index`, `nameLT`, `nameEN`, `id`) VALUES (19, 'asdfa', 'dddd', 19), (20, 'asdfa', 'dddd', 20);"
       console.log (queryString);
     
       var mysql = require('mysql');
