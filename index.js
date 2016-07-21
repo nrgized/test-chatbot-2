4524ce1056874c1717f26907ed9cc621836bfe4b
nrgized <energieee@gmail.com>
2016-07-21 15:48:39 +0300

    one more commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/webhook', function (req, res) {
         res.send('Error, wrong validation token');
     }
 });
-
+ 
 app.post('/webhook/', function (req, res) {
     console.log (req.body);
     messaging_events = req.body.entry[0].messaging;
