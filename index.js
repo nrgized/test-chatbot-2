ab81cc7058661e80ded3b16ef4011873cb279d73
nrgized <energieee@gmail.com>
2016-08-30 15:48:01 +0300

    oen more

--- index.js
+++ index.js
@@ -156,14 +156,13 @@ function getStopsData() {
       // parse html
       var cheerio = require('cheerio'),
       $ = cheerio.load(body);
-
         var script = $('script:contains("var opts")').html();
         script = script.substring(script.indexOf("var opts = {") + 11);
         script = script.substring(0, script.indexOf(";") -1);
         script = script.replace(/\\\//g, "/"); 
-        var opts = {};
-        opts =  JSON.parse(script);
-        console.log(opts);
+        //var opts = {};
+        //opts =  JSON.parse(script);
+        //console.log(opts);
         
 
       $( "ul.zones-list" ).children().each(function(i, elem) {
