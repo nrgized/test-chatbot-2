76bded16fb753622fa5236271882072c7cce041c
nrgized <Audrius@Audriuss-MacBook-Pro.local>
2016-09-12 15:19:29 +0300

    attachments[0]

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.post('/webhook/', function (req, res) {
 
 // check if location is sent
 
-        if (event.message && event.message.attachments) {
-            //var location = event.message.attachments.payload.coordinates;
-            var location = "123";
+        if (event.message && event.message.attachments[0].type == 'location') {
+            var location = event.message.attachments[0].payload.coordinates;
+            //var location = "123";
             sendTextMessage(sender, "location received " + location );
             console.log(event.message.attachments);
         }
