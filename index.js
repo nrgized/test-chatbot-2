1ba1dfd14a124c84bb1880d68be08eb0acbb32b2
nrgized <Audrius@Audriuss-MacBook-Pro.local>
2016-09-13 10:33:10 +0300

    &

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ function getNearestCars(UserLat, UserLng) {
         }
         Destinations = Destinations.substring(0, Destinations.lastIndexOf("|"));
         // googleAPI distance matrix call
-        var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + Origin + 'destinations=' + Destinations + 'mode=walk&language=en-EN&key=' + googleAPIkey;
+        var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + Origin + '&destinations=' + Destinations + '&mode=walk&language=en-EN&key=' + googleAPIkey;
         console.log (url);
 
 
