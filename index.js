636c2a854834e40d02cec2eeac772b044587e783
nrgized <Audrius@Audriuss-MacBook-Pro.local>
2016-09-14 16:14:32 +0300

    fix

--- index.js
+++ index.js
@@ -449,34 +449,32 @@ function sendTextMessage(sender, text) {
 }
 
 function changeThreadSettings() {
-    messageData = {
-    "setting_type" : "call_to_actions",
-    "thread_state" : "existing_thread",
-    "call_to_actions":[
-      {
-        "type":"postback",
-        "title":"Help",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
-      },
-      {
-        "type":"postback",
-        "title":"Start a New Order",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
-      },
-      {
-        "type":"web_url",
-        "title":"View Website",
-        "url":"http://petersapparel.parseapp.com/"
-      }
-    ]
-  };
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/thread_settings',
         qs: {access_token:app.get('page_access_token')},
         method: 'POST',
         json: {
-            message: messageData,
-        }
+          "setting_type" : "call_to_actions",
+          "thread_state" : "existing_thread",
+          "call_to_actions":[
+            {
+              "type":"postback",
+              "title":"Help",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+            },
+            {
+              "type":"postback",
+              "title":"Start a New Order",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
+            },
+            {
+              "type":"web_url",
+              "title":"View Website",
+              "url":"http://petersapparel.parseapp.com/"
+            }
+          ]
+        };
     }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log('thread_settings set');
