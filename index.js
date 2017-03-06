ab24e385ffbe1572c118d346adac1a5f5ab3ab98
nrgized <energieee@gmail.com>
2017-03-06 15:24:22 +0200

    threadsettings

--- index.js
+++ index.js
@@ -657,7 +657,7 @@ function changeThreadSettings() {
     }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log('thread_settings set');
-          console.log(body);
+        //  console.log(body);
           console.log(response);
         }
         if (error) {
